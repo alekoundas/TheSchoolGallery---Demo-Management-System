a39e6457757d8be03e8308b4dfa06fb63a1aa6c7
giger <tsormang@yahoo.gr>
2019-10-17 20:28:30 +0300

    Parallax working

--- Web_Front/App_Start/BundleConfig.cs
+++ Web_Front/App_Start/BundleConfig.cs
@@ -14,8 +14,6 @@ namespace Web_Front
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
@@ -25,6 +23,30 @@ namespace Web_Front
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            //// CUSTOM SCRIPT COLLECTIONS 1 -------------------->>
+            //bundles.Add(new ScriptBundle("~/bundles/Skriptakia").Include(
+            //          "~/js/vendor/bootstrap.min.js",
+            //          "~/js/jquery.ajaxchimp.min.js",
+            //          "~/js/owl.carousel.min.js",
+            //          "~/js/jquery.nice-select.min.js",
+            //          "~/js/jquery.magnific-popup.min.js",
+            //          "~/js/jquery.counterup.min.js",
+            //          "~/js/waypoints.min.js",
+            //          "~/js/main.js"
+            //          ));
+
+            //// CUSTOM STYLE COLLECTIONS  -------------------->>
+            //bundles.Add(new ScriptBundle("~/bundles/Stylakia").Include(
+            //          "~/css/linearicons.css",
+            //          "~/css/owl.carousel.css",
+            //          "~/css/font-awesome.min.css",
+            //          "~/css/nice-select.css",
+            //          "~/css/magnific-popup.css",
+            //          "~/css/bootstrap.css",
+            //          "~/css/main.css"
+            //          ));
+
         }
     }
 }
