15406b69b832b7f00b75c35345a399e1fab7afd7
giger <tsormang@yahoo.gr>
2019-11-26 19:35:00 +0200

    Detailing

--- Web_Front/Controllers/MasterController.cs
+++ Web_Front/Controllers/MasterController.cs
@@ -1,18 +1,8 @@
-﻿using Newtonsoft.Json;
-using RestSharp;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using Web_DomainClasses;
-using Web_DomainClasses.Entities;
-using Web_DomainClasses.Entities.Avatar;
+﻿using System;
 using Web_DomainClasses.Entities.SignalR;
-using Web_DomainClasses.Entities.School;
 using System.Web.Mvc;
 using Web_Services.ApiMapping;
-using System.Net;
 using System.Linq;
-using PagedList;
 
 namespace Web_Front.Controllers
 {
@@ -22,35 +12,9 @@ namespace Web_Front.Controllers
     {
         MessageApiService MessageServ = new MessageApiService();
         //SignalR Required To Return Chat
-        public ActionResult Chat(string currentFilter, string searchString, int? page)
-        {
-            var chat = from sch in MessageServ.GetMessageHistorys()
-                          select sch;
-
-            // PAGE NUMBERS ----------------------------------------------------------------------->>
-            if (searchString != null)
-            {
-                page = 1;
-            }
-            else
-            {
-                searchString = currentFilter;
-            }
-            ViewBag.CurrentFilter = searchString;
-
-            // FILTER --------------------------------------=-------------------------------------->>
-            if (!String.IsNullOrEmpty(searchString))
-            {
-                chat = chat.Where(st => (st.Name.Contains(searchString)));
-            }
-
-            // Number of Pages -------------------------------------------------------------------->>
-            int pageSize = 4;
-            int pageNumber = (page ?? 1);
-            return View(chat.ToPagedList(pageNumber, pageSize));
-
-            // Palio return ------------------------------>>
-            //return View(MessageServ.GetMessageHistorys());
+        public ActionResult Chat()
+        {            
+            return View(MessageServ.GetMessageHistorys());
         }
 
 
